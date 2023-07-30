620e257362e104525654a0accd55cfbfc8587c0d
tamirdennis <tamirdennis@gmail.com>
2023-07-30 16:25:42 +0300

    config from TSA

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 # path of cifar10 data on yout device
-cifar_root = "/data/baijiawang/cifar10_data"
+cifar_root = "data/baijiawang/cifar10_data"
 
 
 # path of the quantized model
-model_root = "../cifar_resnet_quan_8"
-float_model_root = "../cifar_resnet_float"
+model_root = "models/cifar_resnet_quan_8"
+float_model_root = "models/cifar_resnet_float"
 
